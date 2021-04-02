7265db76be94d42fe3f470a71836e0a88a63757f
Tomi Haapalainen <tomi.haapalainen@protonmail.com>
2021-04-02 16:01:28 +0300

    Update success page.

--- src/Purchase/SuccessPage.js
+++ src/Purchase/SuccessPage.js
@@ -5,14 +5,15 @@ import strings from "./components/strings";
 
 export default function SuccessPage() {
   return (
-    <Container fluid className="justify-content-center align-items-center">
+    <Container fluid className="mt-5 mx-auto mw-1024">
       <Card style={{ maxWidth: "500px" }}>
         <Card.Header className="bg-primary text-white">
           <Card.Title>{strings.purchaseSuccessful}</Card.Title>
         </Card.Header>
         <Card.Body>
           <Container>
-            <p>{strings.thankYouForPurchasing}</p>
+            <p className="my-2">{strings.thankYouForPurchasing}</p>
+            <p className="my-2">{strings.welcomeBack}</p>
           </Container>
         </Card.Body>
       </Card>
